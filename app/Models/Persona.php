b424afdc06c1ab972d668881662692c98c54fe96
alejandro-gonzales <alejandro.g280211@gmail.com>
2021-05-20 23:25:53 -0400

    added template and list person

---
+++ app/Models/Persona.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    use HasFactory;
+
+    protected $table = "persona";
+    protected $fillable = ['id', 'nombre', 'apellido', 'ci', 'fecha_nac', 'email', 'direccion', 'celular', 'estado', 'created_at', 'updated_at'];
+
+}
